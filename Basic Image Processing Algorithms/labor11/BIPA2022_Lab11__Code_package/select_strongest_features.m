3d4c28ce9e133fdacda0fa1fded9a302af90536d
budzs <61503191+budzs@users.noreply.github.com>
2023-09-17 11:55:06 +0200

    Delete Basic Image Processing Algorithms/labor11/BIPA2022_Lab11__Code_package directory

--- Basic Image Processing Algorithms/labor11/BIPA2022_Lab11__Code_package/select_strongest_features.m
+++
@@ -1,6 +0,0 @@
-function [FeaturePointsOut] = select_strongest_features(FeaturePoints, upper_limit, min_metric)
-    [~, idx] = sort(FeaturePoints.Metric, 'descend');
-    idx = idx(1:upper_limit);
-    FeaturePointsOut = FeaturePoints(idx);
-    FeaturePointsOut = FeaturePointsOut( FeaturePointsOut.Metric > min_metric);   
-end
